064f092089dffdf690b658e18c5f0ab49cce9f75
jeesukset <jani.liikkanen@gmail.com>
2014-02-09 13:09:49 +0200

    kakkaa

--- src/memorypro/NewJPanel.java
+++ src/memorypro/NewJPanel.java
@@ -29,6 +29,11 @@ public class NewJPanel extends javax.swing.JPanel {
         jButton1 = new javax.swing.JButton();
 
         jButton1.setText("jButton1");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -47,6 +52,11 @@ public class NewJPanel extends javax.swing.JPanel {
                 .addGap(39, 39, 39))
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     // End of variables declaration//GEN-END:variables
